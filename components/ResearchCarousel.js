1cd0dce67a945c7c06ab28ddde03d978d986f85c
Pritam Das Adhikari <pritamdasadhikari8917@gmail.com>
2025-09-04 12:26:53 +0530

    update

--- components/ResearchCarousel.js
+++ components/ResearchCarousel.js
@@ -55,7 +55,7 @@ const ResearchCarousel = () => {
               </p>
             </div>
 
-            {item.status === "published" ? (
+            {item.status === "Published" ? (
               <button
                 className="mt-auto inline-flex items-center justify-center gap-2 border border-[#00629B] text-[#00629B] rounded-full px-5 py-2 text-sm md:text-base font-medium hover:bg-[#00629B] hover:text-white transition-all duration-300"
                 onClick={() => window.open(item.file)}
